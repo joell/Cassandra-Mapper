cc614771da7f6211e610164d4e199b6e7e42d310
Joel Lathrop <joell@sunbelt-software.com>
2011-06-06 10:11:43 -0400

    Set sane default consistency levels.

--- cassandra.rb
+++ cassandra.rb
@@ -5,6 +5,12 @@ require 'cassandra_mapper/document'
 module CassandraMapper
   class << self
     def client
+      if not @is_consistency_set
+        # Note: Replication defaults are set as metadata in the keyspace schema.
+        Cassandra::WRITE_DEFAULTS[:consistency] = Cassandra::Consistency::QUORUM
+        Cassandra::READ_DEFAULTS[:consistency]  = Cassandra::Consistency::QUORUM
+        @is_consistency_set = true
+      end
       Thread.current[:cassandra_mapper_client] ||= Cassandra.new(*client_config)
     end
 
