dc27a9ed1a0b450fdaba25386ce85130c752ee1e
Joel Lathrop <joell@sunbelt-software.com>
2011-06-08 18:28:09 -0400

    PARTIAL FIX: Ordering updates must take into affect changing `group_by' field values.
    Changes on `group_by' are handled in the case that the ordered field
    also changes.  However, if the `group_by' field's value alone changes,
    the ordering is still not updated.

--- cassandra_mapper/ordering/persistence.rb
+++ cassandra_mapper/ordering/persistence.rb
@@ -19,21 +19,29 @@ module CassandraMapper
       module InstanceMethods
         def update_orders
           updated = self.class.orderings
+          # TODO: BUG: This does not handle changes in the group_by field alone.
           updated &= changed  if not new?
           updated.each  do |order_name|
             # NOTE: We use a super column to avoid conflicts on the ordered value
-            group_by = self.class.properties[order_name][:options][:group_by]
+            group_by_field = self.class.properties[order_name][:options][:group_by]
             column_family = "#{self.class.model_name.collection}_by_#{order_name}"
-            row = group_by ? serialize_value(attributes[group_by]) : "\0"
 
             # if the old value was non-nil, then its entry must be removed from 
             #   the ordering
             if not new? and old_val = changes[order_name].first
+              old_row = "\0"
+              if group_by_field
+                old_group_val = changes.has_key?(group_by_field) ?
+                    changes[group_by_field].first :
+                    attributes[group_by_field]
+                old_row = serialize_value(old_group_val)
+              end
               old_col = serialize_value(old_val)
-              CassandraMapper.client.remove(column_family, row, old_col, self.key)
+              CassandraMapper.client.remove(column_family, old_row, old_col, self.key)
             end
 
             # add our key in the ordering at the location for our new ordered value
+            row = group_by_field ? serialize_value(attributes[group_by_field]) : "\0"
             col = serialize_value(attributes[order_name])
             CassandraMapper.client.insert(column_family, row, {col => {self.key => ""}},
                                           :timestamp => timestamp)
