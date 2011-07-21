6ee4ce7ca9a86cfb650556452543c48b0a217f9d
Joel Lathrop <joel.lathrop@gfi.com>
2011-07-21 16:59:50 -0400

    Fix: Properly inherit versioning properties.

--- lib/cassandra_mapper/versioning/properties.rb
+++ lib/cassandra_mapper/versioning/properties.rb
@@ -5,6 +5,14 @@ module CassandraMapper
     module Properties
       extend ActiveSupport::Concern
 
+      def inherited(subclass)
+        super
+        sc_obl_cbs = subclass.instance_variable_get(:@_obliterate_callbacks) || []
+        subclass.instance_variable_set(:@_obliterate_callbacks,
+                                       sc_obl_cbs + _obliterate_callbacks)
+        subclass.versioning_group_by(version_group_field)
+      end
+
       def max_versions(n=5)
         @max_versions ||= n
       end
@@ -21,12 +29,12 @@ module CassandraMapper
       attr_reader :version_group_field
 
       def before_obliterate(*methods)
-        @@_obliterate_callbacks ||= []
-        @@_obliterate_callbacks += methods
+        @_obliterate_callbacks ||= []
+        @_obliterate_callbacks += methods
       end
 
       def _obliterate_callbacks
-        @@_obliterate_callbacks ||= []
+        @_obliterate_callbacks ||= []
       end
     end
   end
